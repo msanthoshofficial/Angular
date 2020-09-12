c097c0cd305b1eda6f437d338093fade815fb95d
santhosh <santhosh.m.2017.ece@ritchennai.edu.in>
2020-09-12 17:52:49 +0530

    Added primeng and a component

--- CRUD/src/app/app.module.ts
+++ CRUD/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
